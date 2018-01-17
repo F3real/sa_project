322df33901d27d969253f5ddbaec335501574cef
F3real <stefan92ff@yandex.com>
2018-01-17 14:11:31 +0100

    Change access service

--- access_service.py
+++ access_service.py
@@ -49,29 +49,35 @@ def handle_auth_message(reader_id, user_id):
     print("Handling RFID request")
     db=MySQLdb.connect(host=DB_HOST, user=DB_USERNAME, passwd=DB_PASSWORD, db=DB_NAME)
     
-    cur = db.cursor()
-    cur.execute("SELECT room_id, type FROM rfid WHERE id=%s LIMIT 1", (reader_id,))
-    res = cur.fetchall()
-    room_id = res[0][0]
-    rfid_type = res[0][1]
+    try:
+        with db.cursor() as cur:
+            cur.execute("SELECT room_id, type FROM rfid WHERE id=%s LIMIT 1", (reader_id,))
+            res = cur.fetchall()
+            room_id = res[0][0]
+            rfid_type = res[0][1]
 
-    now = datetime.datetime.now()
-    day = WEEKDAYS[now.weekday()]
-    hour = now.hour
-  
-    cur.execute("SELECT * FROM permissions WHERE room_id=%s and user_id=%s and day=%s", (room_id, user_id, day))
-    res = cur.fetchall()[0]
-    if hour > res[3] and hour < res[4]:
-        mySocket = socket.socket()
-        mySocket.connect((MSG_SERVER_HOST, MSG_SERVER_PORT))
-        msg = 'GATE {}'.format(room_id)
-        mySocket.send(msg.encode())
-        mySocket.close()
+            now = datetime.datetime.now()
+            day = WEEKDAYS[now.weekday()]
+            hour = now.hour
 
-        #Insert into access log
-        cur.execute("""INSERT INTO `entrylog` (`id`, `room_id`, `time`, `type`, `user_id`) 
-        VALUES (NULL, %s, %s, %s, %s)""", (room_id, now.isoformat(), rfid_type, user_id))
-        db.commit()
+        with db.cursor() as cur:
+            cur.execute("""SELECT * FROM permissions WHERE room_id=%s and user_id=%s and day=%s""",
+                        (room_id, user_id, day))
+            res = cur.fetchall()[0]
+            if hour > res[3] and hour < res[4]:
+                mySocket = socket.socket()
+                mySocket.connect((MSG_SERVER_HOST, MSG_SERVER_PORT))
+                msg = 'GATE {}'.format(room_id)
+                mySocket.send(msg.encode())
+                mySocket.close()
+
+                #Insert into access log
+                with db.cursor() as cur:
+                    cur.execute("""INSERT INTO `entrylog` (`id`, `room_id`, `time`, `type`, `user_id`) 
+                    VALUES (NULL, %s, %s, %s, %s)""", (room_id, now.isoformat(), rfid_type, user_id))
+                    db.commit()
+    finally:
+        db.close()
 
 if __name__ == '__main__':
     Main()
